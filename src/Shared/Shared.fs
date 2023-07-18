e9ab35f04ebf5a3e9d7a32d9d414ece845ec61fa
Mika Qvist <mika.k.qvist@gmail.com>
2023-07-18 14:08:53 +0300

    Initial domain model

--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -4,6 +4,23 @@ open System
 
 type Todo = { Id: Guid; Description: string }
 
+type Money = decimal
+type Account = { Id: Guid; Name: string }
+type Outflow = { FromAccount: Guid }
+type Inflow = { ToAccount: Guid }
+type Transfer = { FromAccount: Guid; ToAccount: Guid }
+
+type TransactionType =
+    | Outflow
+    | Inflow
+    | Transfer
+
+type Transaction =
+    { Id: Guid
+      Amount: Money
+      Type: TransactionType
+      Comment: string }
+
 module Todo =
     let isValid (description: string) =
         String.IsNullOrWhiteSpace description |> not
